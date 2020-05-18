3e23b37c9471946fe0bdf9269a8e6edc98608bde
Matthew Cook <matthewcook13987@gmail.com>
2020-05-18 15:46:10 -0700

    Created seperate .js file for game script and put reference call in HTML.

---
+++ game.js
@@ -0,0 +1,27 @@
+/*
+
+TODO
+- Create switchcase statement for game
+- Create player input
+
+*/
+
+function computerPlay(){
+    let cpuMove = Math.floor(Math.random() * 3)
+
+    if (cpuMove == 0) {
+        return "rock";
+    } else if (cpuMove == 1) {
+        return "paper";
+     } else if (cpuMove == 2) {
+        return "scissors";
+     }
+
+
+
+function rpsGame(){
+    if (a==b)
+    console.log("It's a tie")
+
+}
+}
